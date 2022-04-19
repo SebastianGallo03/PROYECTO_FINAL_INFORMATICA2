b50db16a82baf1b7c1380af1a5036ce85918430a
YisusCrixx <80047446+YisusCrixx@users.noreply.github.com>
2022-04-19 01:25:58 -0500

    nueva clase

---
+++ Proyecto-final-This-polombia/proyectil_en.cpp
@@ -0,0 +1,33 @@
+#include "proyectil_en.h"
+
+proyectil_en::proyectil_en(){
+
+    bullet.load(":/Recursos/bullet.png") ;
+
+    timer_bala = new QTimer() ;
+
+    //identificador=a;
+
+    connect( timer_bala , SIGNAL( timeout() ) , this , SLOT( movimiento_bala() ) ) ;
+
+    timer_bala->start( 20 ) ;
+
+
+}
+
+void proyectil_en::movimiento_bala(){
+
+
+setPos( this->x() - 15 , this->y() ) ;
+
+setPixmap( bullet.scaled( 15 , 15 ) ) ;
+
+    if(this->x()<0)
+    {
+        scene()->removeItem(this);
+
+        delete this;
+    }
+
+
+}
